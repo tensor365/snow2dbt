33bb20eda25ed2a7a52afa480ae072a52f1ce879
DESKTOP-8MM451O\Clement <cparsy@decideom.fr>
2024-12-28 23:46:59 +0100

    maj version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
 name='snow2dbt',
-version='0.1.0',
+version='0.2.0',
 author='Tensor',
 author_email='clementparsy02@gmail.com',
 description='Command Line to reverse engineering a Snowflake Table to DBT Model file',
