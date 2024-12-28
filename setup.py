9c8c207846647e3c0b00422f07fef4b2a22f6e04
DESKTOP-8MM451O\Clement <cparsy@decideom.fr>
2024-12-29 00:05:06 +0100

    ajout readme au setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 setup(
 name='snow2dbt',
-version='0.2.1',
+version='0.2.2',
 author='Tensor',
 author_email='clementparsy02@gmail.com',
 description='Command Line to reverse engineering a Snowflake Table to DBT Model file',
+long_description = open('README.md').read(),
+long_description_content_type="text/markdown",
 packages=find_packages(),
 classifiers=[
 'Programming Language :: Python :: 3',
